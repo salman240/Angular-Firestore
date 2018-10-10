3b226602d7aefde62f63f9c11f3fc31f5cae78a1
Salman Yousaf <salmanyousaf240@gmail.com>
2018-10-10 16:31:43 +0500

    Added firebase config to production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyDc9bALOd8CDujR4sX6hnggxDLlFSPYTyA",
-    authDomain: "angular-firestore-app-6ea42.firebaseapp.com",
-    databaseURL: "https://angular-firestore-app-6ea42.firebaseio.com",
-    projectId: "angular-firestore-app-6ea42",
-    storageBucket: "angular-firestore-app-6ea42.appspot.com",
-    messagingSenderId: "650921267182"
-  }
 };
 
 /*
